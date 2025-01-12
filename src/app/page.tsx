1661b9198511421adafa8520f6a6ab5f44a5e226
kaifayazkhan <kaifayazkhan@gmail.com>
2025-01-12 16:49:30 +0530

    feat: initialized the shadcn ui and added the necessary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import React from 'react';
+
 export default function Home() {
-  return (
-    <div className='flex min-h-screen items-center justify-center font-[family-name:var(--font-geist-sans)]'>
-      <h1 className='text-4xl font-semibold'>Next.js Starter Template</h1>
-    </div>
-  );
+  return <div>Home</div>;
 }
