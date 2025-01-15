8834505b12a7036ffbcb58bbd474216ba7624691
kaifayazkhan <kaifayazkhan@gmail.com>
2025-01-15 23:48:33 +0530

    chore: added logo, css formatting etc.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='bg-white py-8'>
       <div className='container'>
-        <div className='flex items-center justify-between'>
+        <div className='flex flex-col items-center justify-between gap-4 md:flex-row'>
           <div className='text-xl font-semibold'>Kanban Craft</div>
           <p>Copyright 2023. All Rights Reserved.</p>
         </div>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Layout, Trash } from 'lucide-react';
+import { Trash } from 'lucide-react';
 import { Button } from '../ui/button';
+import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 bg-white'>
+    <header className='sticky top-0 z-10 bg-white'>
       <div className='container flex h-16 items-center justify-between'>
         <div className='flex items-center gap-2'>
-          <Layout className='text-3xl' />
-          <div className='text-3xl font-bold'>Kanban Craft</div>
+          <Image
+            src='/logo-black.png'
+            width={210}
+            height={100}
+            alt='Kanban Craft Logo'
+            className='h-auto w-32 object-contain sm:w-52'
+            priority
+          />
         </div>
         <Button variant='destructive'>
           <Trash />
